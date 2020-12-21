14ec55a05b37231d708c34523c45aeaf4d608aa7
Oleh Sydorenko <oleh.sydorenko1994@gmail.com>
2020-12-21 13:20:09 +0200

    fix 1

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import './styles.css';
 import debounce from 'lodash.debounce';
 import getCountries from './fetchCountries' 
 import markupSite from './markup'
+import  {layoutListCountries}  from './markup'
+import { alert, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+defaultModules.set(PNotifyMobile, {});
+
+// --------------------------------
+import '@pnotify/core/dist/BrightTheme.css';
+
+
+// ----------------------------------
+
 const inputRef = document.querySelector('.input-value');
+const listCountriesRef = document.querySelector('.country');
 const articlesContainerRef = document.querySelector('.js-name-country');
+
+// listCountriesRef.addEventListener('click', layoutListCountries(data));
 
 inputRef.addEventListener('input', debounce((evt) => {
     const country = evt.target.value
@@ -11,6 +25,35 @@ inputRef.addEventListener('input', debounce((evt) => {
     articlesContainerRef.innerHTML = ''
 
     getCountries(country).then(data => {
-        markupSite(data)
-    });
-}), 500)
+      if (data) {
+        if (data.length > 10) {
+          alert('Too many mathces found. Please enter a more specific query!');
+        } else if (data.length < 2) {
+          markupSite(data);
+        } else {
+            layoutListCountries(data);
+            const listCountriesRef = document.querySelector('.country');
+            listCountriesRef.addEventListener('click', (evt) => {
+                console.log(evt.target);
+            });
+        }
+      }
+    })
+}), 2000)
+
+
+
+
+
+
+
+
+
+// (data => {
+//         console.log(data.lenght);
+//         console.log(data);
+//         if (data.lenght < 2) {
+//             markupSite(data)
+//         }
+        
+//     });
--- src/markup.js
+++ src/markup.js
@@ -1,7 +1,23 @@
 import nameCountry from './templates/name-country.hbs'
+import nameCountries from './templates/name-countries.hbs'
+
+
 const articlesContainerRef = document.querySelector('.js-name-country');
 
-export default function markupSite (data) {
+const layoutOneCountry = function markupSite (data) {
     const markup = nameCountry(data)
     articlesContainerRef.insertAdjacentHTML('beforeend', markup)
 }
+
+const layoutListCountries = function markupList(data) {
+    const markup = nameCountries(data)
+    articlesContainerRef.insertAdjacentHTML('beforeend', markup)
+}
+
+export default layoutOneCountry;
+
+export {
+  layoutListCountries
+};
+
+
